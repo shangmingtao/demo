595f566a0e420b6ab2b8a37a90cb91c6d73ec926
shangmingtao <shangmingtao@126.com>
2017-09-01 10:40:13 +0800

    feat(Util.java):add stringutil public class

---
+++ Util.java
@@ -0,0 +1,16 @@
+package cn.com.aladdinet.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtil {
+
+	public static final String defaultKey = "linkdata1234567890";
+
+	public static boolean isEmpty(String str) {
+		if (str != null && !str.equals(""))
+			return false;
+		return true;
+
+	}
+}
